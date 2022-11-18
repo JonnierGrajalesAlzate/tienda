b1580aa02580e4763431d9f897b6e406505a4709
JonnierGrajalesAlzate <105441441+JonnierGrajalesAlzate@users.noreply.github.com>
2022-11-18 18:36:37 -0500

    Add files via upload

---
+++ tiendita/script/script.js
@@ -0,0 +1,72 @@
+var PElm = document.getElementById('contarClick')
+var btnElm = document.getElementById('btnClick');
+var btnElm1 = document.getElementById('btnClick1');
+var btnElm2 = document.getElementById('btnClick2');
+var btnElm3 = document.getElementById('btnClick3');
+var btnElm4 = document.getElementById('btnClick4');
+var btnElm5 = document.getElementById('btnClick5');
+var btnElm6 = document.getElementById('btnClick6');
+var btnElm7 = document.getElementById('btnClick7');
+var btnElm8 = document.getElementById('btnClick8');
+var btnElm9 = document.getElementById('btnClick9');
+var btnElm10 = document.getElementById('btnClick10');
+var btnElm11 = document.getElementById('btnClick11');
+var bntElm12 = document.getElementById('contarClick12');
+var btnElm13 = document.getElementById('btnClick13');
+var contar =0;
+btnElm.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm1.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm2.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm3.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm4.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm5.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm6.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm7.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm8.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm9.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm10.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm11.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm12.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
+btnElm13.onclick = function(){
+    contar++;
+    PElm.textContent = contar;
+}
